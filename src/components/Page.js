48157e0db13ed3f81a18d01c20328feb97558f60
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2023-03-05 14:19:37 +0530

    Update Page.js

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,18 +1,15 @@
-import React from 'react';
-import { LocalThemedBox } from './LocalThemedBox';
-import { ThemeContext } from './ThemeProvider';
+import React, { useState } from 'react';
 
-const Page = () => {
-
-    return(
-        <div className={"container"} id="themed-page">
-            <p id="themed-text-container">
-                lorem ipsum dolor iterit n stuff
-            </p>
-            <button className="btn" id="themed-button">Themed Button</button>
-            <LocalThemedBox />
-        </div>
+const ThemeContext = React.createContext()
+const ThemeProvider = (props) =>{
+    const [theme, setTheme] = useState('light')
+    return (
+        <React.Fragment>
+            <ThemeContext.Provider value={{theme, setTheme}}>
+                {props.children}
+            </ThemeContext.Provider>
+        </React.Fragment>
     )
 }
 
-export { Page }
+export {ThemeProvider,ThemeContext}
