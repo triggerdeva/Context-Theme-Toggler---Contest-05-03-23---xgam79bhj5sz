3cba89aaccc05f17efa13419e98e4827090bd223
triggerdeva <120394539+triggerdeva@users.noreply.github.com>
2023-03-05 13:30:55 +0530

    Update ThemeProvider.js

--- src/components/ThemeProvider.js
+++ src/components/ThemeProvider.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 const ThemeContext = React.createContext()
 const ThemeProvider = (props) =>{
-
+    
+    const [state, setState] = useState({
+        theme: "light",
+    });
+    const updateContext = (key,value) => {
+        setState({
+            ...state,
+            [key]:value,
+        });
+    };
     return (
-        <React.Fragment>
-
-        </React.Fragment>
+        <ThemeContext.Provider value={{...state,
+        updateContext }}>
+        {props.childern}
+        </ThemeContext.Provider>
     )
 }
 
